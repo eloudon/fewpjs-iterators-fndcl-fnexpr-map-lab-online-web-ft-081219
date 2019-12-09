acb172e7ae493bf8642d85b088d948239beca79f
Eric Loudon <loudon.eric@gmail.com>
2019-12-09 15:40:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const tutorials = [
 
 const titleCased = tutorials.map(function (tutorial) {
   tutorial = tutorial.split(' ');
-  for (let i = 0; i < tutorial.length; i++) {
+  for (var i = 0; i < tutorial.length; i++) {
     tutorial[i] = tutorial[i].charAt(0).toUpperCase() + tutorial[i].slice(1);
   }
   return tutorial.join(' ');
